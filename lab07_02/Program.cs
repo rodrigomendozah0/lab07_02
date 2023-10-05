c9e4149458360b718346bbf2399d1ee423100fc3
Rodrigo <Rodrigo@HP_RODRIGO>
2023-10-04 20:21:44 -0500

    Agregar archivos de proyecto.

---
+++ lab07_02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab07_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int resultado = 0; 
+
+            for(i = 1; i <= 1000; i++)
+            {
+                resultado = 3 * i;
+                Console.WriteLine("Divisor de Rodrigo Mendoza " + i + ": " + resultado);
+            }
+            Console.ReadKey();
+        }
+    }
+}
